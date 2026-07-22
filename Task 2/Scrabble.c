5813e73ed5d9ee460ab8b24fe737edc5e61fc851
emanmeseilhy-gif <emanmeseilhy@gmail.com>
2026-07-22 21:09:48 +0300

    Create Scrabble.c

---
+++ Task 2/Scrabble.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(char word[]);
+
+int main(void)
+{
+    char word1[100];
+    char word2[100];
+
+    // Get input words from both players
+    printf("Player 1: ");
+    scanf("%s", word1);
+
+    printf("Player 2: ");
+    scanf("%s", word2);
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+
+    return 0;
+}
+
+int compute_score(char word[])
+{
+    int score = 0;
+
+    // Compute and return score for string
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+
+    return score;
+}
